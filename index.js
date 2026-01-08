e363ede06f1676fb6e3159bbd26c61c75769857b
Connor Francis <connorfrancis@live.com>
2026-01-08 14:52:12 -0700

    second and third Q finished

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
+
+//Question 1
+
 function freelancer(){
     //rate = random number * # of possible numbers rounded down + 20 since we start at 20
     let rate = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min + 1) + PRICE_RANGE.min ) //the 200 -20 + 1 dictates how many possible numbers there are (inclusive). The +20 means start at 20.
@@ -22,13 +25,27 @@ function freelancer(){
     let occupationIndex = Math.floor(Math.random() * OCCUPATIONS.length)
     let occupation = OCCUPATIONS[occupationIndex]
 
-    console.log(name)
-    console.log(occupation)
-    console.log(rate)
-    
+
     return {name, occupation, rate}
-
-
 }
 
-freelancer()
+//Question 2
+
+let freelancers = []
+
+for(i=0; i<NUM_FREELANCERS;i++){
+    let newFreelancer = freelancer()
+    freelancers.push({newFreelancer})
+}
+
+console.log(`Length of freelancers array: ${freelancers.length} fellas`)
+console.log(freelancers)
+
+//Question 3 average rate of all freelancers
+
+let avgFreelancerRate = freelancers.reduce((total, num)=>total + num.newFreelancer.rate, 0)/freelancers.length
+
+console.log(avgFreelancerRate);
+
+
+//Question 4
